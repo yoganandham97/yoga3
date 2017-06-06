07c90e511e4e04551cabdcf636a569d1beb0712b
yoganandham97 <yoganandhamcse@gmail.com>
2017-06-06 11:57:45 +0530

    Create pso.java

---
+++ pso.java
@@ -0,0 +1,21 @@
+import.java.io.*;
+class pso
+{
+public Static void main(String args[])
+{
+int a;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+System.out.println("enter the no:");
+if(a<0)
+{
+System.out.println(a+ "negative");
+}
+else if(a>0)
+{
+System.out.println(a+ "posititve");
+}
+else
+{
+System.out.println("zero");
+}
