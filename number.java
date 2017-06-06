9ad7be5c80267fd49ed2d86e69b6fa385290ba3a
yoganandham97 <yoganandhamcse@gmail.com>
2017-06-06 12:01:14 +0530

    Rename pso.java to number.java

---
+++ number.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class pso
+{
+public Static void main(String args[])
+{
+int a;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+if(a<0)
+{
+System.out.println(a+ "negative");
+}
+else if(a>0)
+{
+System.out.println(a+ "posititve");
+}
+else
+{
+System.out.println("zero");
+}
+}
+}
