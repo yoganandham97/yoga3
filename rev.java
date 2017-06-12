52ede03f4a9fbef5d812c1ca0ad3e80930399333
yoganandham97 <yoganandhamcse@gmail.com>
2017-06-12 18:33:11 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class rev
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+String s2="";
+for(int i=0;i<s1.length();i++)
+{
+s2=s2+s1.charAt(i);
+System.out.println(s2);
+}
+}
+}
