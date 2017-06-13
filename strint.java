c9fd70fa6e55112d84b639141225854cd24e17bd
yoganandham97 <yoganandhamcse@gmail.com>
2017-06-13 15:10:29 +0530

    Create strint.java

---
+++ strint.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class strint
+{
+    public static void main(String args[])
+    {
+        String s="12345";
+        int num=Integer.parseInt(s);
+        System.out.println("the number is"+num);
+    }
+}
