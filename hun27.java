790039945b6c73a5042a9a26c1d8c9a777efae44
yoganandham97 <yoganandhamcse@gmail.com>
2017-07-12 11:32:33 +0530

    Create hun27.java

---
+++ hun27.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class hun27
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+String s=in.nextLine();
+int n=s.length();
+if(n%2==0)
+{
+System.out.println(n);
+}
+else
+{
+System.out.println(n-1);
+}
+}
+}
